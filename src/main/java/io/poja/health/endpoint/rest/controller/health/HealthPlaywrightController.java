364092c9654be467c83147991582ceda1edf1b60
Yumii <yumii.saiko@gmail.com>
2026-08-21 10:26:46 +0300

    infra: frontal_fun as zip, worker_fun as container_img

--- src/main/java/io/poja/health/endpoint/rest/controller/health/HealthPlaywrightController.java
+++ src/main/java/io/poja/health/endpoint/rest/controller/health/HealthPlaywrightController.java
@@ -1,12 +1,9 @@
 package io.poja.health.endpoint.rest.controller.health;
 
-import static io.poja.health.endpoint.rest.controller.health.PingController.OK;
-
 import io.poja.health.endpoint.event.EventProducer;
 import io.poja.health.endpoint.event.model.WebsiteTitleCheckRequested;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HealthPlaywrightController {
-
   private final EventProducer<WebsiteTitleCheckRequested> eventProducer;
 
   @GetMapping(value = "/health/playwright")
-  public ResponseEntity<String> check_website_title(
-      @RequestParam(defaultValue = "https://poja.io") String url) {
+  public String check_website_title(@RequestParam(defaultValue = "https://poja.io") String url) {
     eventProducer.accept(List.of(new WebsiteTitleCheckRequested(url)));
-    return OK;
+    return "OK";
   }
 }
